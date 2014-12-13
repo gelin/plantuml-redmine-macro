8918c2e031449d4d07114e7b19d1ac746d682dc7
Denis Nelubin <dnelubin@gmail.com>
2014-12-13 19:25:03 +0600

    Added simplest macro.

--- init.rb
+++ init.rb
@@ -1,8 +1,28 @@
+require 'redmine'
+
 Redmine::Plugin.register :plantuml_macro do
   name 'PlantUML Macro plugin'
   author 'Denis Nelubin'
-  description 'This is a plugin for Redmine to convert PlantUML text into link to images on PlantUML server'
+  description 'This is a plugin for Redmine to convert PlantUML text into link to the image on PlantUML server'
   version '0.0.1'
   url 'https://bitbucket.org/gelin/plantuml-redmine-macro'
   author_url 'http://google.com/+DenisNelubin'
+
+  Redmine::WikiFormatting::Macros.register do
+
+    desc "Converts PlantUML text into link to the image on PlantUML server. Example:\n\n" +
+         "  {{plantuml\n" +
+           "  Bob -> Alice : hello\n" +
+           "}}"
+    macro :plantuml do |obj, args, text|
+      h("Hello world! Object: #{obj.class.name}, " +
+            (args.empty? ? "Called with no argument" : "Arguments: #{args.join(', ')}") +
+            " and " + (text.present? ? "a #{text.size} bytes long block of text." : "no block of text.")
+      )
+    end
+
+  end
+
 end
+
+
