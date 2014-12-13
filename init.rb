8791a8e96deba4d421c19005f14e1157b3e5b3e0
Denis Nelubin <dnelubin@gmail.com>
2014-12-13 17:59:22 +0600

    Added plugin URL.

--- init.rb
+++ init.rb
@@ -3,5 +3,6 @@ Redmine::Plugin.register :plantuml_macro do
   author 'Denis Nelubin'
   description 'This is a plugin for Redmine to convert PlantUML text into link to images on PlantUML server'
   version '0.0.1'
+  url 'https://bitbucket.org/gelin/plantuml-redmine-macro'
   author_url 'http://google.com/+DenisNelubin'
 end
